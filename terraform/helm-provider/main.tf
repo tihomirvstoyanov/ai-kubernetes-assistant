9f041ccea21134eb923aa14443d43f78cf9fb9ed
tihomirvstoyanov <tihomirvstoyanov@gmail.com>
2026-01-16 13:04:59 +0000

    Refactor: Simplify Helm provider configuration

--- terraform/helm-provider/main.tf
+++ terraform/helm-provider/main.tf
@@ -16,11 +16,7 @@ provider "kubernetes" {
   config_path = var.kubeconfig_path
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = var.kubeconfig_path
-  }
-}
+provider "helm" {}
 
 resource "kubernetes_namespace" "ai_log_tool" {
   count = var.create_namespace ? 1 : 0
